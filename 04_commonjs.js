d0d8d83474bd5b706922c116dfdfc9238a72dcc4
RainGorr <anvseven@gmail.com>
2021-01-18 23:32:13 +0800

    关于的 require引入的认识再补充一点

--- 04_commonjs.js
+++ 04_commonjs.js
@@ -17,8 +17,13 @@ console.log(str)
 // exports.name = 'why'
 // exports.age = 18
 
+// 1、被加载的文件在加载的时候会被执行一次
+// require的加载过程是 同步 的，所以的如果加载时间过长，会导致进程阻塞，体验不是甚好
+// 2、多次加载同一个模块，被加载的模块只会执行一次，因为其内部会缓存是否加载过的状态
+// 3、如果存在循环引用，为‘图解构的深度优先遍历’（还有一种是广度优先）
+
 // let data = require('./xxx')
-// console.log(data) // {name: 'why', age: 18}
+// console.log(data) // 结果 ：{name: 'why', age: 18}
 
 
 // 说两句
